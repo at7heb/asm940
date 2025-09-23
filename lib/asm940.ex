cdd23a234e09e40fcff8ce2d6148298fce0cc872
howard <wharbus@icloud.com>
2025-09-23 17:09:43 -0400

    initial "git new asm940"

---
+++ lib/asm940.ex
@@ -0,0 +1,18 @@
+defmodule Asm940 do
+  @moduledoc """
+  Documentation for `Asm940`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Asm940.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/asm940_test.exs
@@ -0,0 +1,8 @@
+defmodule Asm940Test do
+  use ExUnit.Case
+  doctest Asm940
+
+  test "greets the world" do
+    assert Asm940.hello() == :world
+  end
+end
