dcb5edcf2706eb873ab650517aeaa48b82c49e4b
Howard Bussey <howard.bussey+gitmbp16@gmail.com>
2026-01-08 22:27:34 -0500

    testing

---
+++ assemble_all.sh
@@ -0,0 +1,7 @@
+mix compile; read zz
+time mix test test/z1_test.exs; read zz
+time mix test test/z2_test.exs; read zz
+time mix test test/z3_test.exs; read zz
+time mix test test/z4_test.exs; read zz
+time mix test test/z5_test.exs; read zz
+time mix test test/z6_test.exs; read zz
