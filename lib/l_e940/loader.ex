facbadc290eec95d19380e2af095ef933b7c5559
Howard Bussey <howard.bussey+gitmbp16@gmail.com>
2026-01-31 09:56:37 -0500

    relocate symbols when loaded

--- lib/l_e940/loader.ex
+++ lib/l_e940/loader.ex
@@ -1,4 +1,6 @@
 defmodule LE940.Loader do
+  import Bitwise
+
   def process(%LinkEdit{} = state) do
     raise "is this called?"
     state
@@ -12,9 +14,16 @@ defmodule LE940.Loader do
   def load(%LinkEdit{} = state, {stash_addr, execution_addr, path} = _parameters) do
     new_state = %{state | memory_lc: stash_addr, relocation_lc: execution_addr}
     assembly_info = File.read!(path) |> :erlang.binary_to_term([:safe])
-    Map.keys(assembly_info) |> dbg()
     sample("Memory Sample", assembly_info.mem)
     sample("Symbol Sample", assembly_info.symb)
+
+    sample(
+      "Expression Sample",
+      Map.filter(assembly_info.symb, fn {_key, val} -> val.expression_tokens != [] end)
+    )
+
+    new_state = relocate_symbols(new_state, assembly_info.symb)
+    sample("relocated symbols", new_state.symbols)
     new_state
   end
 
@@ -30,4 +39,26 @@ defmodule LE940.Loader do
     IO.puts(label)
     dbg(sample)
   end
+
+  defp relocate_symbols(%LinkEdit{} = state, %{} = symb) do
+    state_symbols =
+      Map.to_list(symb)
+      |> Enum.reduce(%{}, fn {name, value}, new_symbol_map ->
+        Map.put(new_symbol_map, name, relocate_symbol(state.relocation_lc, value))
+      end)
+
+    %{state | symbols: state_symbols}
+  end
+
+  # to relocate, must not be defined by an expression.
+  defp relocate_symbol(
+         execution_lc,
+         %A940.Address{expression_tokens: [], relocation: relocation} = addr
+       ) do
+    %{addr | value: addr.value + relocation * execution_lc &&& addr.mask, relocation: 0}
+  end
+
+  # otherwise return the symbol, and a later phase will resolve the values.
+  # the expression probably involves a symbol from another assembly.
+  defp relocate_symbol(_execution_lc, %A940.Address{} = addr), do: addr
 end
